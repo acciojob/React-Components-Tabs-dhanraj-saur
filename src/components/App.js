be4c13075e75a36d394a53d733858674970a52df
dhanraj-saur <dhananjayverma301@gmail.com>
2023-07-19 10:35:31 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,46 @@
+import React, { useState } from 'react';
+import '../styles/App.css';
 
+function App() {
+    const [activeTab, setActiveTab] = useState('Home');
+
+    const handleTabClick = (tabName) => {
+        setActiveTab(tabName);
+    };
+
+    return (
+        <div className="App">
+            <Tabs activeTab={activeTab} handleTabClick={handleTabClick} />
+            <Viewport activeTab={activeTab} />
+        </div>
+    );
+}
+
+function Tabs({ activeTab, handleTabClick }) {
+    const tabs = ['Home', 'About', 'Features'];
+
+    return (
+        <div className="tabs">
+            {tabs.map((tab) => (
+                <div
+                    key={tab}
+                    className={`tab ${activeTab === tab ? 'active' : ''}`}
+                    id={`${tab}-tab`}
+                    onClick={() => handleTabClick(tab)}
+                >
+                    {tab}
+                </div>
+            ))}
+        </div>
+    );
+}
+
+function Viewport({ activeTab }) {
+    return (
+        <div className="viewport">
+            Pages Go Here: {activeTab}
+        </div>
+    );
+}
+
+export default App;
